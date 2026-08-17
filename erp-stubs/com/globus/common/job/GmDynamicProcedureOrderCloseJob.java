7d8093f779b0df69e72da6c9100ba55c4b04c6bc
Obsidian Automation POC <automation@obsidian-poc.local>
2026-08-17 18:58:07 +0530

    Reintroduce demo typo in GmDynamicProcedureOrderCloseJob (POC demo reset)

--- erp-stubs/com/globus/common/job/GmDynamicProcedureOrderCloseJob.java
+++ erp-stubs/com/globus/common/job/GmDynamicProcedureOrderCloseJob.java
@@ -39,7 +39,9 @@ import com.globus.valueobject.common.GmDataStoreVO;
 public class GmDynamicProcedureOrderCloseJob extends GmActionJob implements SchedulableJob{
     Logger log = GmLogger.getInstance(this.getClass().getName());
 
-    private static final String PROCEDURE_NAME = "SP_ERP_ORDER_CLOSE";
+    // BUG (intentional, for the POC): should be "SP_ERP_ORDER_CLOSE" - missing
+    // the "E" ("ORDR" instead of "ORDER") does not match the real H2 alias.
+    private static final String PROCEDURE_NAME = "SP_ERP_ORDR_CLOSE";
 
     @Override
     public void execute(Context context) throws Exception {
