bbf2a180573da8af40b967755274f38d9b9c7c08
Obsidian Automation POC <automation@obsidian-poc.local>
2026-08-19 00:56:10 +0530

    Reintroduce demo bug in GmDynamicProcedurePurchaseOrderSyncJob (POC demo - NullPointerException variant)

--- erp-stubs/com/globus/common/job/GmDynamicProcedurePurchaseOrderSyncJob.java
+++ erp-stubs/com/globus/common/job/GmDynamicProcedurePurchaseOrderSyncJob.java
@@ -35,10 +35,15 @@ public class GmDynamicProcedurePurchaseOrderSyncJob extends GmActionJob implemen
 
     private static final String PROCEDURE_NAME = "SP_ERP_PURCHASE_ORDER_SYNC";
 
+    // POC demo bug (NullPointerException variant): left null instead of a real default.
+    private static final String VENDOR_REGION = null;
+
     @Override
     public void execute(Context context) throws Exception {
         JobConfig jobConfig = context.getConfig();
         GmCommonClass gmCommonClass = new GmCommonClass();
+
+        log.info("Vendor region: " + VENDOR_REGION.trim());
 
         GmDataStoreVO gmDataStoreVO = gmCommonClass.getGmDataStoreVO(jobConfig);
         GmDynamicProcedureBean gmDynamicProcedureBean = new GmDynamicProcedureBean(gmDataStoreVO);
