5f126eaceacd139391bd7b53a748f8ce9823875a
Obsidian Automation POC <automation@obsidian-poc.local>
2026-08-18 13:47:13 +0530

    Fix ERP_CUSTOMER_SYNC_TEST (manual reset for demo)

--- erp-stubs/com/globus/common/job/GmDynamicProcedureCustomerSyncJob.java
+++ erp-stubs/com/globus/common/job/GmDynamicProcedureCustomerSyncJob.java
@@ -33,9 +33,7 @@ import com.globus.valueobject.common.GmDataStoreVO;
 public class GmDynamicProcedureCustomerSyncJob extends GmActionJob implements SchedulableJob{
     Logger log = GmLogger.getInstance(this.getClass().getName());
 
-    // BUG (intentional, for the POC): should be "SP_ERP_CUSTOMER_SYNC" - missing
-    // the "O" ("CUSTMER" instead of "CUSTOMER") does not match the real H2 alias.
-    private static final String PROCEDURE_NAME = "SP_ERP_CUSTMER_SYNC";
+    private static final String PROCEDURE_NAME = "SP_ERP_CUSTOMER_SYNC";
 
     @Override
     public void execute(Context context) throws Exception {
